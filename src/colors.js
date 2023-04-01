12b700430c206fae34047f7f585746061eee1e73
faizulislamfair <faizulislamfair@gmail.com>
2023-04-01 19:18:47 +0600

    style: brand color setup

---
+++ src/colors.js
@@ -0,0 +1,7 @@
+import './variables.css';
+
+export const PRIMARY_COLOR = 'var(--primary-color)';
+export const SECONDARY_COLOR_1 = 'var(--secondary-color-1)';
+export const SECONDARY_COLOR_2 = 'var(--secondary-color-2)';
+export const ACCENT_COLOR_1 = 'var(--accent-color-1)';
+export const ACCENT_COLOR_2 = 'var(--accent-color-2)';
