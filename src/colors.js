413c55c818243ff8d7ac405e9dae50dbe77ff0b7
faizulislamfair <faizulislamfair@gmail.com>
2023-04-01 22:27:24 +0600

    fixed: changed color variable names, added a newline at EOF

--- src/colors.js
+++ src/colors.js
@@ -1,7 +1,10 @@
 import './variables.css';
 
 export const PRIMARY_COLOR = 'var(--primary-color)';
-export const SECONDARY_COLOR_1 = 'var(--secondary-color-1)';
-export const SECONDARY_COLOR_2 = 'var(--secondary-color-2)';
-export const ACCENT_COLOR_1 = 'var(--accent-color-1)';
-export const ACCENT_COLOR_2 = 'var(--accent-color-2)';
+export const SECONDARY_COLOR = 'var(--secondary-color)';
+export const TERTIARY_COLOR = 'var(--tertiary-color)';
+export const ACCENT_PRIMARY_COLOR = 'var(--accent-primary-color)';
+export const ACCENT_SECONDARY_COLOR = 'var(--accent-secondary-color)';
+
+
+
