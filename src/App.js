d7abdad58690e3de5c131b0428cb2cadb1108716
galib <mgalib101@gmail.com>
2023-04-02 23:39:53 +0600

    feat:setup the react-router-dom and import the basics lib from it

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-
+import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import {Home,About,Contact,Error} from './Pages'
-import Navbar from './Components/Navbar';
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-    
-        <Route path="*" element={<Error/>} />
-      </Routes>
-    </BrowserRouter>
-    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
