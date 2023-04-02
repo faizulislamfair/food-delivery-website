b96f66097eb7e89db2a27e9807051f19da28d47b
galib <mgalib101@gmail.com>
2023-04-02 23:04:05 +0600

    feat: react router dome set up with demo pages

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+const Home = ( {children}) => {
+  return (
+    <>
+    Home Page
+    </>
+  )
+}
+
+export default Home
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Error from "./Error";
+
+export {About,Contact,Home,Error}
