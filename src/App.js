419f2c1a52a12a37d412163be1cb4682fa8bdfd6
faizulislamfair <faizulislamfair@gmail.com>
2023-04-03 17:53:03 +0600

    feat: implement navbar logo (FD-23)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { PRIMARY_COLOR } from './colors.js';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header></Header>
     </div>
   );
 }
