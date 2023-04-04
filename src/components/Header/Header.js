edfd55c38bba4771e086f883503992d816b710d3
galib <mgalib101@gmail.com>
2023-04-04 16:35:19 +0600

    fix:align the navbar items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,8 +12,8 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import brand_img from './../../brand.svg'
 import { PRIMARY_COLOR } from '../../colors';
-import { bgcolor } from '@mui/system';
-
+import { Grid } from '@mui/material';
+import { Stack } from '@mui/system';
 
 
 const pages = ['Home', 'Menu', 'Order', 'Blog', 'About', 'Contact'];
@@ -49,126 +49,142 @@ function Header() {
         }}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <IconButton sx={{
-                        py: 3,
-                        px: 16,
-                        display: { xs: 'none', md: 'flex' },
-                        '&:hover': {
-                            backgroundColor: 'transparent',
-                        }
-                    }} disableRipple>
-                        <img src={brand_img} alt="" />
-                    </IconButton>
+                    <Grid container spacing={1}>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-
-                            <MenuIcon sx={{
-                                color: 'black',
-                                px: 2,
-                                py: 1
-                            }} />
-
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' }
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                    <Typography textAlign="center">{page}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-
-                    <IconButton sx={{
-                        display: { xs: 'flex', md: 'none' },
-                        '&:hover': {
-                            backgroundColor: 'transparent',
-                        },
-                        flexGrow: 1
-                    }} disableRipple>
-                        <img src={brand_img} alt="" />
-                    </IconButton>
-
-
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'black', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
-                    </Box>
-
-
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                {/* <Avatar alt="Remy Sharp" src="" /> */}
+                        <Grid item md={3} >
+                            <IconButton sx={{
+                                py: 3,
+                                px: 16,
+                                display: { xs: 'none', md: 'flex' },
+                                '&:hover': {
+                                    backgroundColor: 'transparent',
+                                }
+                            }} disableRipple>
+                                <img src={brand_img} alt="" />
                             </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                    
-                    <Button
-                     sx={{ my: 2, color:'#333',}}
-                     variant="outlined"color='warning'>
-                        Sign-up
-                     </Button>
-                     <Button
-                     sx={{ my: 2,mx:3, color:'#333',background:PRIMARY_COLOR}}
-                     variant="">
-                        Sign-in
-                     </Button>
-                  
+                        </Grid>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+
+                                <MenuIcon sx={{
+                                    color: 'black',
+                                    px: 2,
+                                    py: 1
+                                }} />
+
+                            </IconButton>
+
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' }
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+
+                        <IconButton sx={{
+                            display: { xs: 'flex', md: 'none' },
+                            '&:hover': {
+                                backgroundColor: 'transparent',
+                            },
+                            flexGrow: 1
+                        }} disableRipple>
+                            <img src={brand_img} alt="" />
+                        </IconButton>
+
+                        {/* Nav bar page show for the md and lg screen  */}
+                        <Grid item md={6} >
+                            <Stack direction="column"
+                                justifyContent="center"
+                                alignItems="center">
+                                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                                    {pages.map((page) => (
+                                        <Button
+                                            key={page}
+                                            onClick={handleCloseNavMenu}
+                                            sx={{ my: 2, color: 'black', display: 'block' }}
+                                        >
+                                            {page}
+                                        </Button>
+                                    ))}
+                                </Box>
+                            </Stack>
+                        </Grid>
+
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    {/* <Avatar alt="Remy Sharp" src="" /> */}
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center">{setting}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <Grid item md={3}>
+                        <Stack direction="row"
+                                justifyContent="center"
+                                alignItems="center">
+                                
+                            <Button
+                                sx={{ my: 2, color: '#333', }}
+                                variant="outlined" color='warning'>
+                                Sign-up
+                            </Button>
+                            <Button
+                                sx={{ my: 2, mx: 3, color: '#333', background: PRIMARY_COLOR }}
+                                variant="">
+                                Sign-in
+                            </Button>
+                            </Stack>
+                        </Grid>
+
+                    </Grid>
                 </Toolbar>
             </Container>
         </AppBar>
