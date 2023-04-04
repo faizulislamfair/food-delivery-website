f8009a143f819571f6a5456c7f53bbd078774746
faizulislamfair <faizulislamfair@gmail.com>
2023-04-04 16:03:46 +0600

    feat: setup file routing structure (FD-47)

---
+++ src/routes/RouteNotFound/RouteNotFound.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './RouteNotFound.css';
+import Container from '@mui/material/Container';
+
+
+const RouteNotFound = () => {
+    return (
+        <Container sx={{
+            marginY: 12,
+            padding: 12,
+            backgroundColor: '#F0BE90',
+            fontSize: 35,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            border: 'solid 2px #F0BE90',
+            borderRadius: '15px',
+            width: '50%'
+        }}>
+            Route Not Found: <span style={{ color: '#ffffff' }}>404 Error!</span>
+        </Container>
+    );
+};
+
+export default RouteNotFound;
