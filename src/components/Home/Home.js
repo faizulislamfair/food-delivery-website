79ae1f8305b7319fe6996e3498d16521bb58f56a
faizulislamfair <faizulislamfair@gmail.com>
2023-04-05 17:09:37 +0600

    feat: implement banner (FD-27, 29, 45)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
 import BannerText from './../BannerText/BannerText';
+import Banner from './../Banner/Banner';
+
 
 const Home = () => {
     return (
         <div>
             <BannerText></BannerText>
+            <Banner></Banner>
         </div>
     );
 };
