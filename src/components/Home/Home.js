c1e9a2790b1073eb86dd03d4ac58c4f39995fd63
faizulislamfair <faizulislamfair@gmail.com>
2023-04-05 22:09:21 +0600

    fixed: about us serial layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Home.css';
 import BannerText from './../BannerText/BannerText';
 import Banner from './../Banner/Banner';
-
+import About from './../About_us/About';
 
 const Home = () => {
     return (
         <div>
             <BannerText></BannerText>
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
