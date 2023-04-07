7e814cbca7e2d8aca928b852cf0c083e458f046e
faizulislamfair <faizulislamfair@gmail.com>
2023-04-07 16:03:42 +0600

    feat: implemented featured dish section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './Home.css';
 import BannerText from './../BannerText/BannerText';
 import Banner from './../Banner/Banner';
-import About from './../About_us/About';
 import BannerDivider from './../BannerDivider/BannerDivider';
+import About from './../About_us/About';
+import FeaturedDishes from './../FeaturedDishes/FeaturedDishes';
 import Testimonial from '../Testimonial/Testimonial';
+
 
 const Home = () => {
     return (
         <div>
-            <BannerText></BannerText>
-            <Banner></Banner>
-            <BannerDivider></BannerDivider>
-            <About></About>
-            <Testimonial/>
+            <BannerText />
+            <Banner />
+            <BannerDivider />
+            <About />
+            <FeaturedDishes />
+            <Testimonial />
         </div>
     );
 };
